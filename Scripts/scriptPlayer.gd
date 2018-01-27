3e13766001652f02c45a148b68c1fed0d2546b76
Hexanix <damyan.dichev@abv.bg>
2018-01-27 06:31:00 +0200

    Layer Switch v2

--- Scripts/scriptPlayer.gd
+++ Scripts/scriptPlayer.gd
@@ -11,10 +11,12 @@ func get_layer_node(var layer):
 	return get_tree().get_root().get_node("World").get_node("Layers/" + layer);
 	
 	
-func layerOnOff(var event, var layer):
+func layerOn(var layer):
 		if get_layer_node(layer).is_hidden():
 			get_layer_node(layer).show();
-		else:
+			
+func layerOff(var layer):
+		if get_layer_node(layer).is_hidden() == false:
 			get_layer_node(layer).hide();
 			
 
@@ -22,8 +24,10 @@ func layerOnOff(var event, var layer):
 func layerActivate(var layerToShow, var layerOther_1, var layerOther_2, var event, var action):
 	if event.is_action_pressed(action):
 		if get_layer_node(layerOther_1).is_hidden() || get_layer_node(layerOther_2).is_hidden():
-			layerOnOff(event, layerToShow);
-		
+			if get_layer_node(layerToShow).is_hidden():
+				layerOn(layerToShow);
+			elif (get_layer_node(layerOther_2).is_hidden() == false || get_layer_node(layerOther_1).is_hidden() == false):
+				layerOff(layerToShow);
 		
 	
 	
