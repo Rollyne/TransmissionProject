a7f71808008e63e899ab4f20483cb90a67886d2d
Hexanix <damyan.dichev@abv.bg>
2018-01-28 07:32:38 +0200

    Fixed layer collision

--- Scripts/scriptLayerSwitch.gd
+++ Scripts/scriptLayerSwitch.gd
@@ -1,6 +1,10 @@
 extends Node2D
 onready var globals = get_node("/root/globals");
 
+var sceneMechanical = load("res://Levels/LayerScenes/Mechanical.tscn")	
+var sceneLight = load("res://Levels/LayerScenes/Light.tscn")
+var sceneMorse = load("res://Levels/LayerScenes/Morse.tscn")
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -11,23 +15,36 @@ func _ready():
 func get_layer_node(var layer):
 	return get_tree().get_root().get_node("World").get_node("Layers/" + layer);
 	
+
+func get_layer_activate_node(var layer):
+	return get_tree().get_root().get_node("World").get_node("LayersActivate/" + layer);
+	
 	
 func layerOn(var layer):
-		if get_layer_node(layer).is_hidden():
-			get_layer_node(layer).show();
+	var node;
+	if get_layer_node(layer) == null:
+			if layer == "Mechanical":
+				node = sceneMechanical.instance();
+			elif layer == "Morse":
+				node = sceneMorse.instance();
+			elif layer == "Light":
+				node = sceneLight.instance();
 			
+			add_child(node);
+	
+	
 func layerOff(var layer):
-		if get_layer_node(layer).is_hidden() == false:
-			get_layer_node(layer).hide();
+		if get_layer_node(layer) != null:
+			get_tree().get_root().get_node("World").get_node("Layers").remove_child(get_layer_node(layer));
 			
 
 	
 func layerActivate(var layerToShow, var layerOther_1, var layerOther_2, var event, var action):
 	if event.is_action_pressed(action):
-		if get_layer_node(layerOther_1).is_hidden() || get_layer_node(layerOther_2).is_hidden():
-			if get_layer_node(layerToShow).is_hidden():
+		if get_layer_node(layerOther_1) == null || get_layer_node(layerOther_2) == null:
+			if get_layer_node(layerToShow) == null:
 				layerOn(layerToShow);
-			elif (get_layer_node(layerOther_2).is_hidden() == false || get_layer_node(layerOther_1).is_hidden() == false):
+			elif (get_layer_node(layerOther_2) != null || get_layer_node(layerOther_1) != null):
 				
 				layerOff(layerToShow);
 		
@@ -38,7 +55,7 @@ func _input(event):
 	layerActivate("Mechanical", "Light", "Morse", event, "controlMechanical");
 	layerActivate("Morse", "Light", "Mechanical", event, "controlMorse");
 	
-	if (get_layer_node("Morse").is_hidden()):
+	if (get_layer_node("Morse") == null):
 		globals.setLayerMorseActive(false);
 	else:
 		globals.setLayerMorseActive(true);
